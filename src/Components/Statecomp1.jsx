251994b907e78c049ead20cb2288d48083488301
RGCENA <raviganesh387@gmail.com>
2024-09-03 05:22:59 +0530

    React

--- src/Components/Statecomp1.jsx
+++ src/Components/Statecomp1.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 const StateComp1 = () => {
-
+    console.log("working good");
     const[state,setstate] = useState("throw out the heaven and earth")
 
     return(
