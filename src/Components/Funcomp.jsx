be2f5a488983fbc6e76a0eae1ea45693d21408ee
RGCENA <raviganesh387@gmail.com>
2024-09-03 02:08:49 +0530

    react

---
+++ src/Components/Funcomp.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+
+
+export class ClassComp extends Component {
+render(){
+    return(
+        <div>
+        <h1>
+        Tree Structure
+        </h1>
+        
+        </div>
+    )
+}
+}
+
+export const FunComp = () => {
+    return(
+        <div>
+        <Child1/>
+        <Child2/>
+        <Child3/>
+           
+        </div>
+    )
+}
+
+export const Child1 = () => {
+    return(
+        <div>
+            <h1>
+                Child-1
+            </h1>
+            <GrandChild1/>
+        </div>
+    )
+}
+
+export const GrandChild1 = () => {
+    return(
+        <div>
+            <h1>
+                GrandChild-1
+            </h1>
+            <GreatGrandChild1/>
+        </div>
+    )
+}
+export const GreatGrandChild1 = () => {
+    return(
+        <div>
+            <h1>
+                GreatGrandChild-1
+            </h1>
+        </div>
+    )
+}
+
+export const Child2 = () => {
+    return(
+        <div>
+            <h1>
+                Child2
+            </h1>
+            <GrandChild2/>
+            <GrandChild3/>
+        </div>
+    )
+}
+
+export const GrandChild2 = () => {
+    return(
+        <div>
+            <h1>
+                GrandChild-2
+            </h1>
+        </div>
+    )
+}
+export const GrandChild3 = () => {
+    return(
+        <div>
+            <h1>
+                GrandChild-3
+            </h1>
+        </div>
+    )
+}
+
+export const Child3 = () => {
+    return(
+        <div>
+            <h1>
+                Child-3
+            </h1>
+            <GrandChild4/>
+        </div>
+    )
+}
+
+export const GrandChild4 = () =>{
+    return(
+        <div>
+            <h1>
+                GrandChild-4
+            </h1>
+        </div>
+    )
+}
+
