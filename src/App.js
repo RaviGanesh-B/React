cffce999d7079f1a98eebf5fa4d3a8e13c2eec17
RGCENA <raviganesh387@gmail.com>
2024-09-03 05:13:26 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 // import { ClassComp, FunComp } from './Components/Funcomp';
-import StateComp from './Components/Statecomp';
+// import StateComp from './Components/Statecomp';
+import StateComp1 from './Components/Statecomp1';
 
 function App() {
   return (
     <div className="App">
-     <StateComp/>
+     <StateComp1/>
     </div>
   );
 }
---
+++ src/Components/Statecomp1.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+const StateComp1 = () => {
+
+    const[state,setstate] = useState("throw out the heaven and earth")
+
+    return(
+        <div style = {{
+            width:"100%",
+            height:"100vh",
+            alignItems:"center",
+            display:"flex",
+            flexDirection:"column",
+            justifyContent:"center",
+            gap:"10px"
+        }}>
+            <p>{state}</p>
+            <button onClick={()=>setstate("I alone am the honored one")} style={{width:"100px"}}>CLICK</button>
+        </div>
+    ) 
+}
+
+export default StateComp1;
